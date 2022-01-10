851a8862727c4ea0fd41ad7d5554fda82a703284
LiRein11 <sergey11200011@mail.ru>
2022-01-10 15:34:25 +0500

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,12 @@ $(function(){
     autoplaySpead: 2000
   });
   
+  $(".star").rateYo({
+    rating: 3.6,
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });
+
 });
