50408613f7e4b017e56fa328dc4c71d1ad3d0375
LiRein11 <sergey11200011@mail.ru>
2022-01-13 19:46:42 +0500

    added product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 $(function(){
 
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    vertical: true,
+    graggable: false
+  });
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    graggable: false,
+    arrows: false,
+    fade: true
+  });
+
   $('.shop-content__filter-btn').on('click', function(){
     $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
     $(this).addClass('shop-content__filter-btn--active');
@@ -13,7 +28,7 @@ $(function(){
     $('.product-item').removeClass('product-item--list');
   });
   
-  $('.select-style').styler();
+  $('.select-style, .product-one__item-num').styler();
 
   $('.filter-price__input').ionRangeSlider({
     type: "double",
